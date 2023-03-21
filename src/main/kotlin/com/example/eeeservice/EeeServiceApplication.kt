4c08606b713ecb064fc4989f72befdc4d834378a
문정민 <moondeve41@gmail.com>
2023-03-21 10:05:34 +0900

    first commit

---
+++ src/main/kotlin/com/example/eeeservice/EeeServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.eeeservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EeeServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<EeeServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/eeeservice/EeeServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.eeeservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EeeServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
