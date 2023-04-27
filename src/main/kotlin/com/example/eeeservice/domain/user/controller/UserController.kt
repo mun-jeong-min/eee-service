4d535428cbd8ce9a688378b9891dda68bd9d338f
문정민 <moondeve41@gmail.com>
2023-04-27 16:59:10 +0900

    EDIT :: user api controller

--- src/main/kotlin/com/example/eeeservice/domain/user/controller/UserController.kt
+++ src/main/kotlin/com/example/eeeservice/domain/user/controller/UserController.kt
@@ -1,17 +1,27 @@
 package com.example.eeeservice.domain.user.controller
 
-import com.example.eeeservice.domain.user.exception.EmailNotFoundException
-import org.springframework.web.bind.annotation.GetMapping
+import com.example.eeeservice.domain.user.controller.dto.CodeRequest
+import com.example.eeeservice.domain.user.controller.dto.EmailRequest
+import com.example.eeeservice.domain.user.service.UserAuthService
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/user")
-class UserController {
+class UserController (
+        private val userAuthService: UserAuthService
+) {
 
-    @GetMapping
-    fun get(): String {
-        throw EmailNotFoundException.EXCEPTION
+    @PostMapping
+    fun get(@RequestBody request: EmailRequest) {
+        userAuthService.execute(request)
+    }
+
+    @PostMapping("/signup")
+    fun code(@RequestBody request: CodeRequest):String {
+        return userAuthService.signup(request)
     }
 
 }
